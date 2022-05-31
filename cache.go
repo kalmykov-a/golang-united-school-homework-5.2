b691d2fcd0e716466ee2450ff4102b1c6049b81c
kalmykov-a <kalmykoff93@mail.ru>
2022-05-31 21:43:02 +0300

    4 try

--- cache.go
+++ cache.go
@@ -11,14 +11,14 @@ type item struct {
 	deadline time.Time
 }
 
-func NewCache(key string, value string, deadline time.Time) Cache {
-	return Cache{map[string]item{key: {value, deadline}}}
+func NewCache() Cache {
+	return Cache{data: make(map[string]item)}
 }
 
 func (c *Cache) Get(key string) (string, bool) {
 	//returns the value associated with the key and the boolean ok (true if exists, false if not), if the deadline of the key/value pair has not been exceeded yet
 	val, ok := c.data[key]
-	if ok && c.data[key].deadline.After(time.Now()) {
+	if ok && (c.data[key].deadline.After(time.Now()) || c.data[key].deadline.IsZero()) {
 		return val.value, true
 	} else {
 		return "", false
@@ -27,15 +27,15 @@ func (c *Cache) Get(key string) (string, bool) {
 
 func (c *Cache) Put(key, value string) {
 	//places a value with an associated key into cache. Value put with this method never expired(have infinite deadline). Putting into the existing key should overwrite the value
-	c.data[key] = item{value, time.Now().Add(time.Hour * 1000)}
+	c.data[key] = item{value, time.Time{}}
 }
 
 func (c *Cache) Keys() []string {
 	//should return the slice of existing (non-expired keys)
 	var res []string
-	for k, _ := range c.data {
-		if c.data[k].deadline.After(time.Now()) {
-			res = append(res, k)
+	for key, _ := range c.data {
+		if c.data[key].deadline.After(time.Now()) || c.data[key].deadline.IsZero() {
+			res = append(res, key)
 		}
 	}
 	return res
