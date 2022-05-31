4d4f31ccf681837aa82308af1f3e362521b316a3
kalmykov-a <kalmykoff93@mail.ru>
2022-05-31 16:47:02 +0300

    first try

--- cache.go
+++ cache.go
@@ -2,22 +2,52 @@ package cache
 
 import "time"
 
+const (
+	MinUint uint = 0 // binary: all zeroes
+
+	// Perform a bitwise NOT to change every bit from 0 to 1
+	MaxUint = ^MinUint // binary: all ones
+)
+
 type Cache struct {
+	data map[string]item
 }
 
-func NewCache() Cache {
+type item struct {
+	value    string
+	deadline time.Time
+}
+
+func NewCache(key string, value string, deadline time.Time) Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	//returns the value associated with the key and the boolean ok (true if exists, false if not), if the deadline of the key/value pair has not been exceeded yet
+	val, ok := c.data[key]
+	if ok && c.data[key].deadline.After(time.Now()) {
+		return val.value, true
+	} else {
+		return "", false
+	}
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	//places a value with an associated key into cache. Value put with this method never expired(have infinite deadline). Putting into the existing key should overwrite the value
+	c.data[key] = item{value, time.Unix(int64(MaxUint), 0)}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	//should return the slice of existing (non-expired keys)
+	var res []string
+	for k, _ := range c.data {
+		if c.data[k].deadline.After(time.Now()) {
+			res = append(res, k)
+		}
+	}
+	return res
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.data[key] = item{value, deadline}
 }
